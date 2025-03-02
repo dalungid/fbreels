80270bc4b6505c3bb0212b6854a956222beffd8b
zxip47 <zxip47@gmail.com>
2025-03-02 17:53:16 +0700

    update new metode

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import os
 import re
 import time
 import requests
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
-from ffmpeg import FFmpeg
+from selenium import webdriver # type: ignore
+from selenium.webdriver.common.by import By # type: ignore
+from selenium.webdriver.support.ui import WebDriverWait # type: ignore
+from selenium.webdriver.support import expected_conditions as EC # type: ignore
+from selenium.webdriver.chrome.options import Options # type: ignore
+from ffmpeg import FFmpeg # type: ignore
 from dotenv import load_dotenv
 
 load_dotenv()
